e3d951c48dc834cee45517ba1776575796db12d8
Mika Tuupola <tuupola@appelsiini.net = Mika Tuupola tuupola@appelsiini.net>
2003-06-09 08:49:17 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_12_0'.

---
+++ RELEASE_0_12_0/tests/postgres.php
@@ -0,0 +1,17 @@
+<?php
+
+/* $Id$ */
+
+require_once('DB.php');
+
+$user = '';
+$pass = '';
+$host = 'localhost';
+$db_name = 'test';
+$dsn = "pgsql://$user:$pass@$host/$db_name";
+
+$dbh = DB::connect($dsn, true);
+if (DB::isError($dbh)) { 
+    print 'skip';
+}
+?>
