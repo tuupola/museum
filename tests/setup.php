bcb8fa870165c1de1d256e0df421b47bc23e0609
Mika Tuupola <tuupola@appelsiini.net = Mika Tuupola tuupola@appelsiini.net>
2003-10-08 14:33:44 +0000

    * fixing typo

--- tests/setup.php
+++ tests/setup.php
@@ -3,7 +3,7 @@
 //define('DRIVER_DSN', '');
 //define('DRIVER_DSN', 'pgsql://postgres@pgsql.example.com/test');
 //define('DRIVER_DSN', 'mysql://root@localhost/test');
-//define('DRIVER_DSN', 'mssql://sa:hippa@MSSQLSERVER/pubs');
+//define('DRIVER_DSN', 'mssql://sa:foo@MSSQLSERVER/pubs');
 define('DRIVER_DSN', 'sqlite://dummy:@localhost/' . getcwd() . DIRECTORY_SEPARATOR . 'test.db' );
 
 ?>
