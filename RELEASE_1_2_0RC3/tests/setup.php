bb34f30ecc1b4cc052912bf12351a16d3b02f075
Mika Tuupola <tuupola@appelsiini.net = Mika Tuupola tuupola@appelsiini.net>
2004-05-27 19:44:56 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_2_0RC3'.

---
+++ RELEASE_1_2_0RC3/tests/setup.php
@@ -0,0 +1,9 @@
+<?php
+
+//define('DRIVER_DSN', '');
+//define('DRIVER_DSN', 'pgsql://postgres@pgsql.example.com/test');
+//define('DRIVER_DSN', 'mysql://root@localhost/test');
+//define('DRIVER_DSN', 'mssql://sa:foo@MSSQLSERVER/pubs');
+define('DRIVER_DSN', 'sqlite://dummy:@localhost/' . getcwd() . DIRECTORY_SEPARATOR . 'test.db' );
+
+?>
